51f2af651447be5e8e006acb876d54451f5ceb03
Atila jr <atilla.junior33@gmail.com>
2023-01-09 16:14:50 -0300

    Segunda Formula

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import entities.MyComparator;
 import entities.Product;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class App {
@@ -14,7 +15,17 @@ public class App {
     list.add(new Product("Notebook", 1200.0));
     list.add(new Product("Tablet", 400.0));
 
-    list.sort(new MyComparator());
+    //aki eu fiz uma vareavel comp que vai ser um objeto do tipo Comparator<Product>
+    Comparator<Product> comp = new Comparator<Product>() {
+      // e para definir esse objeto eu criei uma claas anonima aki mesmo dentro do programa principal
+      //eu nao vou mais presisar ter uma class separada
+      @Override
+      public int compare(Product p1, Product p2) {
+        return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+      }
+    };
+
+    list.sort(comp);
 
     for (Product p : list) {
       System.out.println(p);
